c7e31d47cf58f04d4f0ffcc7c8b5b2065f5effb1
Tawdi <ahmedtawdi00@gmail.com>
2025-04-22 23:32:34 +0100

    ajoute controller et route pour books

---
+++ src/controllers/books.controller.js
@@ -0,0 +1,62 @@
+const booksService = require('../services/books.service')
+
+exports.index = async (req, res) => {
+    try {
+        const books = await booksService.all();
+        res.json(books);
+    } catch (error) {
+        res.status(500).json({ 'error': error.message });
+    }
+}
+
+exports.show = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const book = await booksService.getById(id);
+        if (!book) {
+            return res.status(404).json({ message: 'Livre non trouvé.' });
+        }
+        res.json(book);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.store = async (req, res) => {
+    const { title, author, date_publication, category_id } = req.body;
+    try {
+        const result = await booksService.create({ title, author, date_publication, category_id });
+        const book = await booksService.getById(result.insertId);
+        res.status(201).json(book);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+exports.update = async (req, res) => {
+    const { id } = req.params;
+    const { title, author, date_publication, category_id } = req.body;
+    try {
+        const result = await booksService.update(id, { title, author, date_publication, category_id });
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Livre introuvable.' });
+        }
+        const updatedBook = await booksService.getById(id);
+        res.json(updatedBook);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.destroy = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await booksService.delete(id);
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Livre introuvable.' });
+        }
+        res.json({ message: 'Livre supprimé avec succès.' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
--- src/server.js
+++ src/server.js
@@ -6,11 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const booksRoutes = require('./routes/books.routes');
+
 const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Bienvenue sur DevBook API');
 });
+
+app.use('/api/books', booksRoutes);
 
 app.listen(PORT, () => {
     console.log(`Serveur en cours sur http://127.0.0.1:${PORT}`);
