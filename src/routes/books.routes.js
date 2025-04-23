ba33a4f790a70b9ea24410c77aa908dfffd32a7b
Tawdi <ahmedtawdi00@gmail.com>
2025-04-23 21:23:56 +0100

    modifier le route store

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/books.controller');
+const upload = require('../middleware/upload');
 
 router.get('/', controller.index);
 
 router.get('/:id', controller.show);
 
-router.post('/', controller.store);
+router.post('/', upload.single('image'), controller.store);
 
 router.put('/:id', controller.update);
 
